006fbe150c96a7a1bd56efbbcaebc6e540e57d24
Vishnu Vijayan <vvijayan@purdue.edu>
2017-05-26 13:23:52 +0530

    Create movies component

---
+++ src/app/components/movies/movies.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'movies',
+  moduleId: module.id,
+  templateUrl: 'movies.component.html',
+})
+export class MoviesComponent  {
+    
+}
